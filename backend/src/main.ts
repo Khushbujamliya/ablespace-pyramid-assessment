b9d0080bed3284c64bad9f45a6166b1c68f46fa7
Khushbujamliya <khushbujamliya@gmail.com>
2026-08-17 01:29:39 +0530

    fix: allow additional Vercel domain aliases in CORS config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://ablespace-pyramid-assessment.vercel.app'],
+    origin: ['http://localhost:3001', 'https://ablespace-pyramid-assessment.vercel.app', 'https://ablespace-pyramid-assessment-git-main-khushbu-jamliyas-projects.vercel.app',
+      'https://ablespace-pyramid-assessment-nlu2vtewe.vercel.app',],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
